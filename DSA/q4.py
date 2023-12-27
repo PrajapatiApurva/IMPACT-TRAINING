5acda1297fbc9be5e6179d704e85def21a373adb
apurva-6265 <apurva18092003@gmail.com>
2023-12-27 11:44:07 +0530

    adding notes of python in PF lecture

--- DSA/q4.py
+++ DSA/q4.py
@@ -1,6 +1,7 @@
 # write a program to count the number of ways to divide a number in 4 parts
-1 1 1 5
-2 2 2 2
-1 2 2 3 
-1 1 3 3
-1 1 2 4
+# it's for input 8:
+# 1 1 1 5
+# 2 2 2 2
+# 1 2 2 3 
+# 1 1 3 3
+# 1 1 2 4
