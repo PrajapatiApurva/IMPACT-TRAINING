c39db2a10381c7625bfc713252750d36501376ae
apurva-6265 <apurva18092003@gmail.com>
2024-02-11 12:24:54 +0530

    Starting the day with Queue

--- DSA/Queue/Python/queue1.py
+++ DSA/Queue/Python/queue1.py
@@ -1,56 +1,94 @@
 class Queue:
-    
     def __init__(self) -> None:
-        self.n=int(input("Enter the length of the queue: "))
-        self.queue=[]*self.n
-        self.front=-1
-        self.rear=-1
-        self.run()
-    def enque(self,num):
-        if(self.rear==self.n-1):
-            print("Queue Overflow Condition Raised...")
-        
-        self.rear+=1
-        self.queue.append(num)
-        
-        if(self.rear==0):
-            self.front=0
+        self.data=[]
+    def enque(self,data):
+        self.data.append(data)
     def deque(self)->int:
-        if (self.front==-1 and self.rear==-1):
-            print("Empty Queue.....")
-        
-        elif(self.front!=self.rear):
-            temp=self.queue.pop(0)
-            self.front+=1
-            return temp
-            
-        elif(self.front==self.rear):
-            temp=self.queue.pop(0)
-            self.front=-1
-            self.rear=-1
-            return temp
-    
-    def peek(self):
-        if (self.front==-1 and self.rear==-1):
-            print("Empty Queue.....")
-        
+        if self.data:
+            return self.data.pop(0)
+    def peek(self)->int:
+        if self.data:
+            return self.data[0]    
+    def display(self):
+        if self.data:
+            print(*self.data)
         else:
-            return self.queue[self.front-1]
+            print("Queue is Empty...")
+    
+def run():
+    queue=Queue()
+    flag=True
+    while(flag):
+        choice=int(input("Select the choice:1.enque 2.deque 3.display 4.peek 4.exit\n"))
+        if(choice==1):
+            value=int(input("Enter the number you want to Enter: "))
+            queue.enque(value)
+        elif(choice==2):
+            print(queue.deque(),"is Popped...")
+        elif(choice==3):
+            queue.display()  
+        elif(choice==4):
+            print(queue.peek(),"is at the front of Queue")          
+        elif(choice==4):
+            flag=False
+        else:
+            print("Enter Valid choice...")  
+
+run()      
+    
+# class Queue:
+    
+#     def __init__(self) -> None:
+#         self.n=int(input("Enter the length of the queue: "))
+#         self.queue=[]*self.n
+#         self.front=-1
+#         self.rear=-1
+#         self.run()
+#     def enque(self,num):
+#         if(self.rear==self.n-1):
+#             print("Queue Overflow Condition Raised...")
+        
+#         self.rear+=1
+#         self.queue.append(num)
+        
+#         if(self.rear==0):
+#             self.front=0
+#     def deque(self)->int:
+#         if (self.front==-1 and self.rear==-1):
+#             print("Empty Queue.....")
+        
+#         elif(self.front!=self.rear):
+#             temp=self.queue.pop(0)
+#             self.front+=1
+#             return temp
             
-    def run(self):
-        flag=True
-        while(flag):
-            choice=int(input("Select the choice:1.enque 2.deque 3.peek 4.exit\n"))
-            if(choice==1):
-                value=int(input("Enter the number you want to push: "))
-                self.enque(value)
-            elif(choice==2):
-                print(self.deque(),"is Popped...")
-            elif(choice==3):
-                print(self.peek(),"is at the front of Queue")
-            elif(choice==4):
-                flag=False
-            else:
-                print("Enter Valid choice...")        
+#         elif(self.front==self.rear):
+#             temp=self.queue.pop(0)
+#             self.front=-1
+#             self.rear=-1
+#             return temp
+    
+#     def peek(self):
+#         if (self.front==-1 and self.rear==-1):
+#             print("Empty Queue.....")
+        
+#         else:
+#             return self.queue[self.front-1]
+            
+#     def run(self):
+#         flag=True
+#         while(flag):
+#             choice=int(input("Select the choice:1.enque 2.deque 3.peek 4.exit\n"))
+#             if(choice==1):
+#                 value=int(input("Enter the number you want to push: "))
+#                 self.enque(value)
+#             elif(choice==2):
+#                 print(self.deque(),"is Popped...")
+#             elif(choice==3):
+#                 print(self.peek(),"is at the front of Queue")
+#             elif(choice==4):
+#                 flag=False
+#             else:
+#                 print("Enter Valid choice...")        
                 
-option=Queue()
+# option=Queue()
